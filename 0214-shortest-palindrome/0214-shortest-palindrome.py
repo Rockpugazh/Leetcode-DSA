611c8d79655210931fae873e414a3a18fbb3ea0f
Pugazh <119952918+Rockpugazh@users.noreply.github.com>
2026-09-07 14:35:06 +0530

    Time: 1218 ms (9.78%), Space: 19.7 MB (76.73%) - LeetHub

---
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def shortestPalindrome(self, s):
+        n = len(s)
+
+        for i in range(n, -1, -1):
+            if s[:i] == s[:i][::-1]:
+                return s[i:][::-1] + s
+
+        return ""
