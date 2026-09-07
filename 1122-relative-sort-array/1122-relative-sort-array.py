28c63eb22bca5ffd5556a825c5e45d749de112b1
Pugazh <119952918+Rockpugazh@users.noreply.github.com>
2026-09-07 14:22:02 +0530

    Time: 4 ms (26.04%), Space: 19.4 MB (51.17%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def relativeSortArray(self, arr1, arr2):
+        result = []
+
+        for num in arr2:
+            while num in arr1:
+                result.append(num)
+                arr1.remove(num)
+
+        arr1.sort()
+        result.extend(arr1)
+
+        return result
